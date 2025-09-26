df0f226a3897496ad7b367ef36d143138c182f00
Leonardo Guardado <leonardo.guardado.488@my.csun.edu>
2025-09-26 15:19:43 -0700

    Exercise 2 Done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,10 +17,29 @@ public class Main {
         //Exercise #1
         Point startPoint;
         startPoint = new Point(10,20);     //Instantiating and Initializing startPoint
-        System.out.println(startPoint.x);        //Printing startPoint x value
+        System.out.println("Starting point x value: " + startPoint.x);        //Printing startPoint x value
 
         startPoint.move(5,-3);      //Using Point move method
         startPoint.printState();                 //Using Point printState method
+        System.out.println();
+
+//--------------------------------------------------------------------------------------------
+        //Exercise #2
+        int originalValue = 50;
+        attemptToChangePrimitive(originalValue);
+        System.out.println("New value: " + originalValue);
+        System.out.println();
+        /*
+        The value of originalValue stayed the same even after the method because changes to the parameter
+        (originalValue) within the method are only local and do not affect the original value. In
+        order to get the change from the method, you will need to make a new variable and make the method
+        return the new change to said variable.
+        Example: int changedValue = attemptToChangePrimitive(originalValue) where attemptToChangePrimitive
+        has been changed to return a value
+         */
+
+//------------------------------------------------------------------------------------------------
+
 
 
 
